fdd692150cc69d96dca52d1ba39c3fb9d43fd6bd
Traves Theberge <traves.theberge@gmail.com>
2024-06-26 14:54:16 -0400

    Updated Tailwind elements in project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'], // Updated to reflect the correct path
+  content: ['./*.html'], // Make sure this matches the files where you want Tailwind to apply
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans', 'sans-serif'], // This sets Noto Sans as the default sans-serif font
+      },
+    },
   },
-  plugins: [],
 }
